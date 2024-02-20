e6315ffe4bfb711148ad623a5e7d856cdff05698
Tal Ben-Nun <tbennun@gmail.com>
2024-02-19 22:55:05 -0800

    Add language definition and sample

---
+++ spatialstencil/syntax/astnodes.py
@@ -0,0 +1,12 @@
+
+class Node:
+    """
+    Abstract class representing an AST node for spatial stencils.
+    """
+    pass
+
+class Program(Node):
+    """
+    Root node of a stencil program AST.
+    """
+    pass
